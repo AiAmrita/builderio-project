7f2ed09d3e281689cedff7e55fc6bf8d96e06d46
AiAmrita <aiamrita654@gmail.com>
2025-06-29 19:07:18 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>StrictMode>,
+)</StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>StrictMode>,
+)</StrictMode>createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>StrictMode>,
+)</StrictMode>createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+)  </StrictMode>StrictMode>,
 )</StrictMode>
